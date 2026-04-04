a70013ba665c330582408c56b05655e4ce0b42a3
CitroenGames <54474809+CitroenGames@users.noreply.github.com>
2026-04-04 10:03:15 +0200

    fixed bug yes

--- Game/Editor/src/panels/SceneHierarchyPanel.hpp
+++ Game/Editor/src/panels/SceneHierarchyPanel.hpp
@@ -8,7 +8,11 @@ public:
     entt::entity selected_entity = entt::null;
 
     // Draw the hierarchy panel. Pass a dirty flag pointer so changes can trigger BVH rebuild.
-    void draw(entt::registry& registry, bool* out_dirty = nullptr);
+    // out_unsaved: set to true when any scene mutation occurs.
+    void draw(entt::registry& registry, bool* out_dirty = nullptr, bool* out_unsaved = nullptr);
+
+    // Duplicate an entity, selecting the copy. Returns the new entity.
+    entt::entity duplicateEntity(entt::registry& registry, entt::entity source);
 
 private:
     char m_filter_buf[256] = {0};
